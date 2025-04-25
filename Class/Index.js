dd878e029617b9e5b1b1e4fe8cb28f27498fa23e
Armykookie35 <mercyojogo@gmail.com>
2025-04-24 23:01:39 -0700

    changes

--- Class/Index.js
+++ Class/Index.js
@@ -11,7 +11,8 @@ const Index = () => {
         keyExtractor={(item) => item.letter}
         contentContainerStyle={styles.listContainer}
         renderItem={({ item }) => (
-          <View style={[styles.card, { backgroundColor: '#000' }]}>
+          <View style={[styles.card, { backgroundColor: 'red',alignItems:'center',justifyContent:'center' }]}>
+            <Text style={styles.text}>{item.name}</Text>
           </View>
         )}
       />
@@ -26,15 +27,19 @@ const styles = StyleSheet.create({
   card: {
     width: '28%',
     padding: 20,
-    borderRadius: 100,
+    // borderRadius: 100,
     margin: 10,
     alignItems: 'center',
-    backgroundColor: 'black',
+    backgroundColor: 'red',
     height:90,
     width:90
   },
   letter: {
     fontSize: 28,
     fontWeight: 'bold',
+  },
+  text:{
+    fontSize:20,
+    fontWeight:'bold'
   },
 });
