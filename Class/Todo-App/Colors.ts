c67bdc7f5ece3a0bfe30b6dc3d31dd585ac92048
Armykookie35 <mercyojogo@gmail.com>
2025-06-12 12:06:24 -0700

    F

---
+++ Class/Todo-App/Colors.ts
@@ -0,0 +1,12 @@
+const lightTheme = {
+    background: '#ffffff',
+    text: '#000000',
+    inputBackground: '#f0f0f0',
+  };
+  
+  const darkTheme = {
+    background: '#121212',
+    text: '#ffffff',
+    inputBackground: '#1e1e1e',
+  };
+  
